4afa244b6dab6123e92cd1d8c723917e75e4dc71
Danny <nassre@gmail.com>
2024-02-19 03:00:23 -0500

    added preprocessing to remove redunant text in emails

--- utils.py
+++ utils.py
@@ -34,24 +34,48 @@ def find_urls(string):
     return set([''.join(url).lower() for url in urls])
 
 
+def find_phone_nums(string):
+    phone_num_pattern = r'[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}'
+    return re.findall(phone_num_pattern, string)
+
+
+# def get_sender(email):
+#     name = email.get('senderName', '')
+#     address = email.get('senderEmailAddress', '').lower()
+#     if not address:
+#         if match := find_email_addresses(name):
+#             address = match[0].lower()
+#     return (name, address)
+
+
+# def get_recipients(email):
+#     recipients = set()
+#     r_names, r_addrs = [], []
+#     for k in ('displayTo', 'displayCC'):
+#         if v := email.get(k):
+#             r_names.extend([name.strip() for name in v.split(';')])
+#     for r in email.get('recipients', []):
+#         r_addrs.append(r['smtpAddress'].lower())
+#     r_names.extend([None for x in range(len(r_addrs) - len(r_names))])
+#     recipients.update([(name, addr)
+#                        for name, addr in zip(r_names, r_addrs)])
+#     return recipients
+
 def get_sender(email):
+    """Returns (name, address)"""
     name = email.get('senderName', '')
     address = email.get('senderEmailAddress', '').lower()
     if not address:
         if match := find_email_addresses(name):
             address = match[0].lower()
-    return (name, address)
+    return name, address
 
 
 def get_recipients(email):
-    recipients = set()
-    r_names, r_addrs = [], []
-    for k in ('displayTo', 'displayCC'):
-        if v := email.get(k):
-            r_names.extend([name.strip() for name in v.split(';')])
+    "Returns (name, address)"
+    recipients = []
     for r in email.get('recipients', []):
-        r_addrs.append(r['smtpAddress'].lower())
-    r_names.extend([None for x in range(len(r_addrs) - len(r_names))])
-    recipients.update([(name, addr)
-                       for name, addr in zip(r_names, r_addrs)])
+        recipients.append([None, r['smtpAddress'].lower()])
+    if len(recipients) == 1:
+        recipients[0][0] = email.get('displayTo')
     return recipients
